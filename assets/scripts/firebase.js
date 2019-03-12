961afb0618ec0df7775da1e533ba9e3c09ec2c06
jstevens79 <jstevens79@users.noreply.github.com>
2019-03-12 15:08:31 -0500

    adding firebase

---
+++ assets/scripts/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAl_ivk9jeCwqIPtiD0bbTBPrgiSQDa0R4",
+  authDomain: "plan-it-project.firebaseapp.com",
+  databaseURL: "https://plan-it-project.firebaseio.com",
+  projectId: "plan-it-project",
+  storageBucket: "plan-it-project.appspot.com",
+  messagingSenderId: "250401634632"
+};
+firebase.initializeApp(config);
