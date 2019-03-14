95d6a727cd94589d3785da6c2227955c43fb8fb3
mrCrawford <noreply>
2019-03-14 18:42:21 -0500

    There are a few more things I could do, namely convert the hours format into nonmilitary time.  However, I think this is mostly there, and we may not need any additional functionality.

--- assets/scripts/yelp.js
+++ assets/scripts/yelp.js
@@ -5,22 +5,46 @@
 //API Key
 //R0lQ1MRLKKrC83brhVmjZ6grB1ExTxiuQvIOCVfpq2jZN_2BsKcAvkq-oA98Z6gqa7zUszdE8BtXMvkLB6HvQ5M7f4ThNh6NHuAfjtsxeF7pge-EUAFkPMd_ARGEXHYx
 
-
-
-
 //Global variables
+var proxyURL = "https://cors-anywhere.herokuapp.com/"
 
+
+//Global variables for the initial getYelpResults() ajax call
 var businessSearch = "/businesses/search?";
 var apiKey = "API_KEY=R0lQ1MRLKKrC83brhVmjZ6grB1ExTxiuQvIOCVfpq2jZN_2BsKcAvkq-oA98Z6gqa7zUszdE8BtXMvkLB6HvQ5M7f4ThNh6NHuAfjtsxeF7pge-EUAFkPMd_ARGEXHYx";
 var locationURL = "&location="
 var locationCity = "37209";
-var proxyURL = "https://cors-anywhere.herokuapp.com/"
 
 
 
-//Function for interacting with Yelp api and returning the results
+//This function performs an ajax call to Yelp using the business id.  This ajax call returns information such as business hours for the specific 
+//restaurant.
 
-function getYelpResults(boolFlag, searchType, key, searchParam_1, searchParam_2) {
+function getBusinessHours(id) {
+
+    var busHoursURL = "https://api.yelp.com/v3/businesses/" + id;
+    console.log('does stuff', id);
+    console.log(busHoursURL);
+
+    $.ajax({
+        url: proxyURL + busHoursURL,
+        method: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer R0lQ1MRLKKrC83brhVmjZ6grB1ExTxiuQvIOCVfpq2jZN_2BsKcAvkq-oA98Z6gqa7zUszdE8BtXMvkLB6HvQ5M7f4ThNh6NHuAfjtsxeF7pge-EUAFkPMd_ARGEXHYx');
+        }
+
+    }).then(function (busHoursResponse) {
+
+        postDetailsToDOM(busHoursResponse);
+
+    })
+
+}
+
+//This function performs an ajax call to Yelp using the zip code or city name and returns a list of restaurants along with other misc data (i.e. rating, number of 
+//ratings, cost, etc.)
+
+function getYelpResults(searchType, key, searchParam_1, searchParam_2) {
     var queryURL = "https://api.yelp.com/v3" + searchType + key + searchParam_1 + searchParam_2;
 
     console.log(queryURL);
@@ -36,50 +60,117 @@ function getYelpResults(boolFlag, searchType, key, searchParam_1, searchParam_2)
 
         console.log(response);
 
-        if(boolFlag){
 
-       postToDOM(response);
-
-        }
-
-        else{
-
-            return response;
-        }
+        postToDOM(response);
 
     })
+
 }
 
 
-//Function for posting the yelp responses to the DOM
+//This function posts the Yelp api results to the DOM
 
 function postToDOM(apiResponse) {
 
-
-
-
-    var todaysDate = new Date();
-    var today = todaysDate.getDay();
-
+    console.log(apiResponse);
     for (var i = 0; i < apiResponse.businesses.length; i++) {
 
-        var newRow = $("<tr>").append(
+        var newRow = $("<tr data-id=" + apiResponse.businesses[i].id + ">").append(
             $("<td>").text(apiResponse.businesses[i].name),
             $("<td>").text(apiResponse.businesses[i].display_phone),
             $("<td>").text(apiResponse.businesses[i].rating),
             $("<td>").text(apiResponse.businesses[i].review_count),
-            $("<td>").text(apiResponse.businesses[i].price)
+            $("<td>").text(apiResponse.businesses[i].price),
+            $("<td>").text(apiResponse.businesses[i].location.display_address)
+
         )
+        newRow.addClass("detailsButton");
 
         $('#yelp-results').append(newRow);
+
     }
 
 }
 
 
-$(window).on("load", function () {
+//This function posts the details of each business id (at this point it is just hours the business is open).  
+//Since I am unsure of where we will want to implement this in the DOM, I simply console logged the hours.  It
+//should be easy enough to convert each console log to a DOM manipulation if we choose.
 
-     var apiResponse = getYelpResults(true, businessSearch, apiKey, locationURL, locationCity);
-     
+function postDetailsToDOM(busDetails) {
+
+    var dayArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    var openDay = 0;
+
+    for (var i = 0; i < dayArray.length; i++) {
+
+        if (i <= busDetails.hours[0].open[busDetails.hours[0].open.length - 1].day) {
+
+            if (i === busDetails.hours[0].open[openDay].day) {
+
+                console.log(dayArray[busDetails.hours[0].open[openDay].day] + ":");
+                console.log("Opens: " + busDetails.hours[0].open[openDay].start);
+                console.log("Closes: " + busDetails.hours[0].open[openDay].end);
+                console.log("");
+                openDay++;
+            }
+
+            else {
+
+                console.log(dayArray[i] + ": closed");
+                console.log("");
+            }
+
+        }
+        else {
+            console.log(dayArray[i] + ": closed");
+            console.log("");
+        }
+    }
+
+}
+
+
+function convertHours(convertThisTime) {
+
+    var convertedTime = "";
+    var firstTwoChar = convertThisTime.toString().slice(0, 2);
+    meridianComp = parseInt(firstTwoChar);
+
+    if (meridianComp > 11) {
+        convertThisTime += " PM";
+    }
+    else {
+        convertThisTime += " AM";
+    }
+
+    if(meridianComp > 12){
+        meridianComp -= 12;
+    }
+
+    var merConv = meridianComp.toString();
+
+    convertThisTime.splice(0,2,merConv);
+
+    return convertThisTime;
+
+
+
+}
+
+
+
+
+$(document).ready(function () {
+
+    getYelpResults(businessSearch, apiKey, locationURL, locationCity);
+
+    $(document).on("click", ".detailsButton", function (event) {
+        var busId = $(this).data("id");
+        console.log(busId);
+        getBusinessHours(busId);
+    })
+
 
 })
