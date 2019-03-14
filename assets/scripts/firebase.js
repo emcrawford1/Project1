0754a9e2142cc0cef9ebc8ff7005e5a175a9853a
jstevens79 <jstevens79@users.noreply.github.com>
2019-03-13 19:56:49 -0500

    getting authorization to work

--- assets/scripts/firebase.js
+++ assets/scripts/firebase.js
@@ -12,37 +12,63 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 
-// var uiConfig = {
-//   signInSuccessUrl: '<url-to-redirect-to-on-success>',
-//   signInOptions: [
-//     // Leave the lines as is for the providers you want to offer your users.
-//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-//     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-//     firebase.auth.GithubAuthProvider.PROVIDER_ID,
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-//     firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-//   ],
-//   // tosUrl and privacyPolicyUrl accept either url string or a callback
-//   // function.
-//   // Terms of service url/callback.
-//   tosUrl: '<your-tos-url>',
-//   // Privacy policy url/callback.
-//   privacyPolicyUrl: function() {
-//     window.location.assign('<your-privacy-policy-url>');
-//   }
-// };
+var uiConfig = {
+  signInSuccessUrl: '/datacalls.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+};
 
-// // Initialize the FirebaseUI Widget using Firebase.
-// var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// // The start method will wait until the DOM is loaded.
-// ui.start('#firebaseui-auth-container', uiConfig);
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+var currentUser = null;
+var userEmail = null;
+var userName = null;
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    $('#firebaseui-auth-container').hide();
+    userName = user.displayName;
+    userEmail = user.email;
+    currentUser = user.uid;
+    user.getIdToken().then(function(accessToken) {
+      var userExists = false
+      database.ref('users').once('value', function(snapshot) {
+        snapshot.forEach(function(childSnaphot) {
+          if (childSnaphot.val().userID === currentUser) {
+            userExists = true;
+          }
+        })
+      }).then(function() {
+        if (!userExists) {
+          database.ref('users').push({
+            userID: currentUser,
+            userName: userName,
+            userEmail: userEmail
+          })
+        }
+      })
+        
+    })
+
+  } else {
+    console.log('logged out');
+    $('#firebaseui-auth-container').show();
+    $('#sign-out').hide()
+  }
+})
 
 
+$('#sign-out').click(function(e) {
+  e.preventDefault()
+  firebase.auth().signOut()
+})
 
-
-// user authenetication...
 
 var currentUser = 'jstevens'
 
